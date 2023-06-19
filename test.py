2622c548d7cc0ee39da81207128ce1f2f52ab4b4
bzhang2 <bzhang2@apple.com>
2023-06-19 00:10:53 -0700

    Add initial files

---
+++ test.py
@@ -0,0 +1,5 @@
+import aws
+
+#aws.insertBlog("test title 2", "test content 2", "e793419b-17db-4938-a719-db8bcb929225", ["https://s3.us-west-2.amazonaws.com/www.sjturtles.com/sea-shore-png-1.png", "https://s3.us-west-2.amazonaws.com/www.sjturtles.com/tropical-island-png.png"])
+#aws.queryBlogs()
+print(aws.scanBlogs())
